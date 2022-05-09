f613ce84db160606524c8c02e096a1c00e0256a3
sophia501098 <47913523+sophia501098@users.noreply.github.com>
2022-05-09 10:47:36 +0800

    Update clock-in.py

--- clock-in.py
+++ clock-in.py
@@ -1,18 +1,15 @@
-# -*- coding: utf-8 -*-
-
 # 打卡脚修改自ZJU-nCov-Hitcarder的开源代码，感谢这位同学开源的代码
 
 import requests
+import base64
+import ddddocr
 import json
 import re
 import datetime
 import time
 import sys
-
-
-class DaKa(object):
+class ClockIn(object):
     """Hit card class
-
     Attributes:
         username: (str) 浙大统一认证平台用户名（一般为学号）
         password: (str) 浙大统一认证平台密码
@@ -22,18 +19,17 @@ class DaKa(object):
         self.headers: (dir) 请求头
         sess: (requests.Session) 统一的session
     """
-
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
         self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
         self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
+        self.captcha_url = "https://healthreport.zju.edu.cn/ncov/wap/default/code"
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
         }
         self.sess = requests.Session()
-
     def login(self):
         """Login to ZJU platform"""
         res = self.sess.get(self.login_url, headers=self.headers)
@@ -43,7 +39,6 @@ class DaKa(object):
             url='https://zjuam.zju.edu.cn/cas/v2/getPubKey', headers=self.headers).json()
         n, e = res['modulus'], res['exponent']
         encrypt_password = self._rsa_encrypt(self.password, e, n)
-
         data = {
             'username': self.username,
             'password': encrypt_password,
@@ -51,44 +46,44 @@ class DaKa(object):
             '_eventId': 'submit'
         }
         res = self.sess.post(url=self.login_url, data=data, headers=self.headers)
-
         # check if login successfully
         if '统一身份认证' in res.content.decode():
             raise LoginError('登录失败，请核实账号密码重新登录')
         return self.sess
-
     def post(self):
         """Post the hitcard info"""
         res = self.sess.post(self.save_url, data=self.info, headers=self.headers)
         return json.loads(res.text)
-
     def get_date(self):
         """Get current date"""
         today = datetime.date.today()
         return "%4d%02d%02d" % (today.year, today.month, today.day)
-
     def get_info(self, html=None):
         """Get hitcard info, which is the old info with updated new time."""
         if not html:
             res = self.sess.get(self.base_url, headers=self.headers)
             html = res.content.decode()
-
         try:
             old_infos = re.findall(r'oldInfo: ({[^\n]+})', html)
             if len(old_infos) != 0:
                 old_info = json.loads(old_infos[0])
             else:
                 raise RegexMatchError("未发现缓存信息，请先至少手动成功打卡一次再运行脚本")
-
             new_info_tmp = json.loads(re.findall(r'def = ({[^\n]+})', html)[0])
             new_id = new_info_tmp['id']
             name = re.findall(r'realname: "([^\"]+)",', html)[0]
             number = re.findall(r"number: '([^\']+)',", html)[0]
+            
+            # 2022.05.07
+            ocr = ddddocr.DdddOcr()
+            resp = self.sess.get(self.captcha_url, headers=self.headers)
+            img_bytes = resp.content
+            captcha = ocr.classification(img_bytes)
+            
         except IndexError:
             raise RegexMatchError('Relative info not found in html with regex')
         except json.decoder.JSONDecodeError:
             raise DecodeError('JSON decode error')
-
         new_info = old_info.copy()
         new_info['id'] = new_id
         new_info['name'] = name
@@ -108,9 +103,16 @@ class DaKa(object):
         new_info['jcqzrq'] = ""
         new_info['gwszdd'] = ""
         new_info['szgjcs'] = ""
+        
+        # 2022.05.07
+        new_info['verifyCode'] = captcha
+        
+        # 2021.08.05 Fix 2
+        magics = re.findall(r'"([0-9a-f]{32})":\s*"([^\"]+)"', html)
+        for item in magics:
+            new_info[item[0]] = item[1]
         self.info = new_info
         return new_info
-
     def _rsa_encrypt(self, password_str, e_str, M_str):
         password_bytes = bytes(password_str, 'ascii')
         password_int = int.from_bytes(password_bytes, 'big')
@@ -118,27 +120,18 @@ class DaKa(object):
         M_int = int(M_str, 16)
         result_int = pow(password_int, e_int, M_int)
         return hex(result_int)[2:].rjust(128, '0')
-
-
 # Exceptions
 class LoginError(Exception):
     """Login Exception"""
     pass
-
-
 class RegexMatchError(Exception):
     """Regex Matching Exception"""
     pass
-
-
 class DecodeError(Exception):
     """JSON Decode Exception"""
     pass
-
-
 def main(username, password):
     """Hit card process
-
     Arguments:
         username: (str) 浙大统一认证平台用户名（一般为学号）
         password: (str) 浙大统一认证平台密码
@@ -146,9 +139,7 @@ def main(username, password):
     print("\n[Time] %s" %
           datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     print("🚌 打卡任务启动")
-
-    dk = DaKa(username, password)
-
+    dk = ClockIn(username, password)
     print("登录到浙大统一身份认证平台...")
     try:
         dk.login()
@@ -156,7 +147,6 @@ def main(username, password):
     except Exception as err:
         print(str(err))
         raise Exception
-
     print('正在获取个人信息...')
     try:
         dk.get_info()
@@ -164,23 +154,15 @@ def main(username, password):
     except Exception as err:
         print('获取信息失败，请手动打卡，更多信息: ' + str(err))
         raise Exception
-
-    print('正在为您打卡打卡打卡')
+    print('正在为您打卡')
     try:
         res = dk.post()
         if str(res['e']) == '0':
             print('已为您打卡成功！')
         else:
             print(res['m'])
+            if (res['m'] == '验证码错误'):
+                raise Exception
     except Exception:
         print('数据提交失败')
         raise Exception
-
-
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    try:
-        main(username, password)
-    except Exception:
-        exit(1)
